df8e0fed10be6c370d675a6f8643994b3a0861ba
Dan Knights <danknights@Dans-MacBook-Air-2.local>
2022-04-16 10:55:27 -0500

    switched to os.path.join so that trailing / is optional

--- utils/import_data.py
+++ utils/import_data.py
@@ -15,17 +15,15 @@ def import_data_from_local(reporting_entity,
     for file in os.listdir(path_to_data):
         inventory = file.split('_')[0]
         file_info = file.split('.')[0].strip(inventory).lstrip('_')
-
         if inventory == reporting_entity:
             print(f'Importing {file}')
             if file.endswith('.csv'):
-                df = pd.read_csv(path_to_data + file)
+                df = pd.read_csv(os.path.join(path_to_data,file))
                 data[file_info] = df
             elif file.endswith('.xlsx') | file.endswith('.xls'):
                 f = pd.ExcelFile(path_to_data + file, engine='openpyxl')
                 sheet_names = f.sheet_names
                 sheets = {sheet: f.parse(sheet_name=sheet) for sheet in sheet_names}
                 data.update(sheets)
-
     return data
 
